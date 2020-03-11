3752357928b31b5ee1e44d4686e82461c5b9fae4
Hellcatlk <zouy.fnst@cn.fujitsu.com>
2020-03-11 08:54:19 +0800

    Update config.go

--- kinder/third_party/kind/loadbalancer/config.go
+++ kinder/third_party/kind/loadbalancer/config.go
@@ -25,7 +25,7 @@ import (
 
 // ConfigData is supplied to the loadbalancer config template
 type ConfigData struct {
-	ControlPlanePort uint
+	ControlPlanePort int
 	BackendServers   map[string]string
 	IPv6             bool
 }
@@ -64,6 +64,9 @@ backend kube-apiservers
 // Config returns a kubeadm config generated from config data, in particular
 // the kubernetes version
 func Config(data *ConfigData) (config string, err error) {
+	if 0 > data.ControlPlanePort {
+		return "", errors.Wrap(err, "ControlPlanePort must be bigger than 0")
+	}
 	t, err := template.New("loadbalancer-config").Parse(DefaultConfigTemplate)
 	if err != nil {
 		return "", errors.Wrap(err, "failed to parse config template")
